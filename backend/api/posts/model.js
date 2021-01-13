d9bfaa85101c2f79ebc043d9789a208910359cb8
Tucker <leachtucker@gmail.com>
2021-01-12 21:15:02 -0800

    Built DB helper functions. Built endpoints utilizing said helper functions with express router

--- backend/api/posts/model.js
+++ backend/api/posts/model.js
@@ -1,23 +1,75 @@
 const db = require('../../data/dbConfig');
 
-function findById(id) {
-    return db('posts').where('id', id);
+async function findById (id) {
+    const [ post ] = await db('posts').where('post_id', id);
+    return Promise.resolve(post);
 }
 
-function getPosts() {
+function getPosts () {
     return db('posts');
 }
 
-async function insertPost(postData) {
+async function insertPost (postData) {
     const [ newPostId ] = await db('posts').insert(postData);
 
-    const newPost = findById(newPostId);
+    const newPost = await findById(newPostId);
 
     return Promise.resolve(newPost);
 }
 
+async function deletePost (id) {
+    const delRecords = await db('posts').where('post_id', id).delete();
+
+    // Check that we deleted some records. If not, return null
+    if (!delRecords || delRecords <= 0) {
+        return Promise.resolve(null);
+    }
+
+    return Promise.resolve(true);
+}
+
+async function updatePost (id, changes) {
+    const changedRecords = await db('posts').where('post_id', id).update(changes);
+
+    if (!changedRecords || changedRecords <= 0) {
+        return Promise.resolve(null);
+    }
+
+    const updatedPost = await findById(id);
+
+    return Promise.resolve(updatedPost);
+}
+
+// NEEDS TESTED
+function findPostsByUserId (id) {
+    return db('posts').where('user_id', id);
+}
+
+function findStepsByPostId (id) {
+    return db('steps').where('post_id', id);
+}
+
+async function insertStepByPostId (id, stepData) {
+    const [ newStepId ] = await db('steps').insert({ ...stepData, post_id: id });
+
+    if (!newStepId) {
+        return Promise.resolve(null);
+    }
+
+    const newStep = await db('steps').where('step_id', newStepId);
+
+    return Promise.resolve(newStep);
+}
+
+// END of NEEDS TESTED //
+
 module.exports = {
     findById,
     getPosts,
-    insertPost
+    insertPost,
+    deletePost,
+    updatePost,
+    findPostsByUserId,
+    findStepsByPostId,
+    insertStepByPostId
 }
--- backend/api/posts/router.js
+++ backend/api/posts/router.js
@@ -26,4 +26,61 @@ router.post('/', (req, res) => {
         });
 });
 
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+
+    Posts.updatePost(id, changes)
+        .then(updatedPost => {
+            res.status(200).json(updatedPost);
+        })
+        .catch(() => {
+            res.status(500);
+        })
+});
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+
+    Posts.deletePost(id)
+        .then(deleted => {
+            if (deleted) {
+                res.status(200).json({ message: "Post deleted" });
+            } else {
+                res.status(404).json();
+            }
+        })
+        .catch(() => {
+            res.status(500);
+        })
+});
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    Posts.findById(id)
+        .then(result => {
+            if (!result) {
+                res.status(404);
+            }
+            res.status(200).json(result);
+        })
+        .catch(() => {
+            return res.status(500);
+        })
+});
+
+router.get('/user/:id', (req, res) => {
+    const { id } = req.params;
+
+    Posts.findPostsByUserId(id)
+        .then(results => {
+            res.status(200).json(results);
+        })
+        .catch(() => {
+            res.status(500);
+        })
+});
+
+// ADD END POINTS UTILIZING findPostsByUserId, findStepsByPostId, insertStepByPostId
+
 module.exports = router;
